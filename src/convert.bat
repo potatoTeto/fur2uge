28092c0c71d21e58b40fb6895d1db71d7ff10190
potatoTeto <potato.cos@gmail.com>
2025-05-04 08:00:40 -0700

    Update convert.bat

--- src/convert.bat
+++ src/convert.bat
@@ -2,5 +2,7 @@
 if not exist input mkdir input
 if not exist output mkdir output
 cd input
-for /R %%f in (*.fur) do %~dp0/Fur2Uge.exe --i "%%f" --o "../output/%%~nf.uge"
+for /R %%f in (*.fur) do (
+    "%~dp0Fur2Uge.exe" --i "%%f" --o "..\output\%%~nf.uge"
+)
 pause
